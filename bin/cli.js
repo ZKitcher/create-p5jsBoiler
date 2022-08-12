7f4119671542cd7f82b501bf5e91c4ca6f44efff
Zac Kitcher <zkitcher@Hamiltonmfg.com>
2022-08-12 12:32:22 -0400

    Added create npm script.

---
+++ bin/cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, { stdio: 'inherit' })
+    } catch (e) {
+        console.error(`Failedd to execure ${command}`, e)
+        return false;
+    }
+    return true;
+}
+
+const repoName = precess.argv[2];
+const gitCheckouCommand = `git clone --depth 1 https://github.com/ZKitcher/p5js-Boilerplate  ${repoName}`;
+// const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the Repo with the name ${repoName}`);
+const checkedOut = runCommand(gitCheckouCommand);
+
+if(!checkedOut) precess.exit(-1);
+
+console.log('Done')
