57016f58a4fbf5f41e436bf3945c068b8c4a4e06
ZKitcher <Zachary.k1995@gmail.com>
2026-03-26 14:54:06 -0400

    Updated CLI to decouple git

--- bin/cli.js
+++ bin/cli.js
@@ -1,23 +1,36 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 const runCommand = command => {
     try {
-        execSync(`${command}`, { stdio: 'inherit' })
+        execSync(`${command}`, { stdio: 'inherit' });
     } catch (e) {
-        console.error(`Failedd to execure ${command}`, e)
+        console.error(`Failed to execute ${command}`, e);
         return false;
     }
     return true;
-}
+};
 
 const repoName = process.argv[2];
-const gitCheckouCommand = `git clone --depth 1 https://github.com/ZKitcher/p5js-Boilerplate ${repoName}`;
+if (!repoName) {
+    console.error('Please provide a project name.');
+    process.exit(1);
+}
 
 console.log(`Cloning the Repo with the name ${repoName}`);
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/ZKitcher/p5js-Boilerplate ${repoName}`;
 
-const checkedOut = runCommand(gitCheckouCommand);
+const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log('Done')
+// Remove the .git folder to decouple the repo
+const gitFolder = path.join(process.cwd(), repoName, '.git');
+if (fs.existsSync(gitFolder)) {
+    fs.rmSync(gitFolder, { recursive: true, force: true });
+    console.log('Removed .git folder. You now have a fresh project ready to `git init`.');
+}
+
+console.log('Project creation complete!');
